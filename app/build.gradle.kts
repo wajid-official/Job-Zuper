4c90f34a8a840d8de9d68d11338445f755f5b869
Abdul Wajid <abdul.wajid.con@viacom18.com>
2023-11-25 12:39:03 +0530

    Required dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id ("kotlin-android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -30,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -66,4 +70,14 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.2")
+
+    //Hilt
+    implementation ("com.google.dagger:hilt-android:2.44")
+    add("kapt", "com.google.dagger:hilt-android-compiler:2.44")
+    add("kapt", "androidx.hilt:hilt-compiler:1.0.0")
+    implementation ("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }
